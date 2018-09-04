6677d3562d6ac3bfcd8d4e76e8a8533ade1af0cf
miyat001 <miyat001@morris.umn.edu>
2018-09-04 10:34:53 -0500

    We finished the compile part.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "number arg: $1"
-echo "directly arg: $2"
-tar -xzf NthPrime.tgz -C $2 
-
+#echo "number arg: $1"
+#echo "directly arg: $2"
+tar -xzf NthPrime.tgz -C $2
+cd $2/NthPrime
+gcc main.c nth_prime.c nth_prime.h -o NthPrime
+./NthPrime $1
